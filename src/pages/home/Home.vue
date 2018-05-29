a8a9e580f8051686a1c6316ef4d030c573c71f90
卢红全 <lhqhacker@163.com>
2018-05-29 10:40:31 +0800

    project init

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    hello world
   </div>
 </template>
-
 <script>
-export default {
-  name: 'App'
+export default{
+  name: 'Home'
 }
 </script>
-
 <style>
+
 </style>
