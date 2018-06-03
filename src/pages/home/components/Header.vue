9032052750aadc12edd2ff4e8d345b9a518a31b6
卢红全 <lhqhacker@163.com>
2018-06-03 09:28:23 +0800

    header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header">
-    <div class="header-left"></div>
-    <div class="header-input"></div>
-    <div class="header-right"></div>
+    <div class="header-left">返回</div>
+    <div class="header-input">输入城市/景点/游玩主题</div>
+    <div class="header-right">城市</div>
   </div>
 </template>
 <script>
@@ -10,6 +10,26 @@ export default{
   name: 'HomeHeader'
 }
 </script>
-<style lang="stylus">
-
+<style lang="stylus" scoped>
+  .header
+    display: flex
+    line-height: .86rem
+    background: #00bcd4
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+    .header-input
+      flex: 1
+      margin-top: .12rem
+      margin-left: .2rem
+      height: .64rem
+      line-height: .64rem
+      background: #ffffff
+      border-radius: .1rem
+      color: #cccccc
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
 </style>
