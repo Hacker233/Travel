0240ba3bed364ac658e06559b1164bbd8742dc36
卢红全 <lhqhacker@163.com>
2018-06-03 22:01:42 +0800

    change

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,25 +1,42 @@
 <template>
+<div class="wrapper">
     <swiper :options="swiperOption">
-        <swiper-slide>I'm Slide 1</swiper-slide>
-        <swiper-slide>I'm Slide 2</swiper-slide>
-        <swiper-slide>I'm Slide 3</swiper-slide>
-        <swiper-slide>I'm Slide 4</swiper-slide>
-        <swiper-slide>I'm Slide 5</swiper-slide>
-        <swiper-slide>I'm Slide 6</swiper-slide>
-        <swiper-slide>I'm Slide 7</swiper-slide>
+        <swiper-slide v-for="item of swiperList" :key="item.id">
+            <img class="swiper-img" :src="item.imgUrl">
+        </swiper-slide>
         <div class="swiper-pagination"  slot="pagination"></div>
   </swiper>
+</div>
 </template>
 <script>
 export default {
   name: 'HomeSwiper',
   data () {
     return {
-      swiperOption: {}
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/3b/ef86879aa50e3002.jpg_750x200_2a108508.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/77/f63bd04dd5319602.jpg_750x200_6ba8e0ca.jpg'
+      }]
     }
   }
 }
 </script>
 <style lang="stylus" scoped>
-
+.wrapper >>> .swiper-pagination-bullet-active
+    background-color #ffffff
+.wrapper
+    width 100%
+    height 0
+    overflow hidden
+    padding-bottom 31.25%
+    background-color #eee
+    .swiper-img
+        width 100%
 </style>
