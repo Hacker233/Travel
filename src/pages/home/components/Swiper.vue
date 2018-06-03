d9f9a47428efcdc1cadd53ac20f79c0ad7a44c11
卢红全 <lhqhacker@163.com>
2018-06-03 20:53:11 +0800

    zujian

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,26 @@
+<template>
+    <swiper :options="swiperOption">
+        <swiper-slide>I'm Slide 1</swiper-slide>
+        <swiper-slide>I'm Slide 2</swiper-slide>
+        <swiper-slide>I'm Slide 3</swiper-slide>
+        <swiper-slide>I'm Slide 4</swiper-slide>
+        <swiper-slide>I'm Slide 5</swiper-slide>
+        <swiper-slide>I'm Slide 6</swiper-slide>
+        <swiper-slide>I'm Slide 7</swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+      return {
+          swiperOption: {}
+      }
+  }
+}    
+</script>
+<style lang="styles" scoped>
+
+</style>
+
